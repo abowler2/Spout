72ecfb13055ea335deee5cf8e144a6af68792b35
abowler <abowler2@gmail.com>
2019-09-22 14:01:30 -0700

    made balls shoot out of spout

--- Assets/Scripts/Spout.cs
+++ Assets/Scripts/Spout.cs
@@ -1,24 +1,38 @@
-﻿using System.Collections;
+﻿using System.Dynamic;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Spout : MonoBehaviour
 {
     [SerializeField]
     private KeyCode sprayKey = KeyCode.Space;
+
     [SerializeField]
     private GameObject dropPrefab;
 
-    private void Update {
+    [SerializeField]
+    private Transform SpawnPoint;
 
+    private void Update() {
+        if(Input.GetKey(sprayKey)) {
+            Spray();
+        }
     }
 
-    private void Start {
-
+    private void Spray()
+    {
+        GameObject go = Instantiate(dropPrefab, SpawnPoint.position, Quaternion.identity);
+        go.GetComponent<Rigidbody>().velocity = transform.up * 5.0f;
     }
 
-    private void OnEnable {
+    private void Start() {
 
-    }
+  }
+
+   private void OnEnable() {
+
+   }
 }
 
